c66f1c3e4393e4e644d1c6baacea0c84091a7b41
ahmedanis45 <ahmedanis4546@gmail.com>
2024-04-22 00:15:10 +0500

    done task

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-var sec = document.getElementById("sec");
-
-
-var date = new Date("16 june 2024").getTime();
-var date2 = new Date().getTime()
-var diff = date - date2;
-
 function time() {
-    var cal1 = Math.floor(diff / (1000 * 3600 * 24));
-    var day = document.getElementById("day").innerHTML = cal1;
-    
-    var hour = document.getElementById("hour");
-    var cal2 = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    hour.innerHTML = cal2;
-    console.log(cal2);
-    
-    var min = document.getElementById("min");
-    var diff3 = Math.floor((date - date2) / (1000 * 60 ));
-    min.innerHTML = diff3;
+    var date = new Date("16 june 2024").getTime();
+    var date2 = new Date().getTime()
+    var diff = date - date2;
 
-// var min = Math.floor((diff % (1000 * 60 * 60 ) / (1000 * 60)));
-// var sec = Math.floor((diff % (1000 * 60)) / 1000);
+    var day = Math.floor(diff / (1000 * 3600 * 24));
+    document.getElementById("day").innerHTML = day;
+    
+    var hour = Math.floor((diff % (1000 * 3600 * 24)) / (1000 * 3600));
+    document.getElementById("hour").innerHTML = hour;
+    
+    var min = Math.floor((diff & (1000 * 3600)) / (1000 * 60 ));
+    document.getElementById("min").innerHTML = min;
+
+    var sec = Math.floor((diff % (1000 * 60))  / 1000 );
+    document.getElementById("sec").innerHTML = sec;
 
 }
 time()
-// setInterval(time, 1000);
+setInterval(time, 1000);
